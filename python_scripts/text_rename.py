c449002e2ad19d7b3d2da364986ddcffd14c4dbb
Sonali Luthar <sonaliluthar@gmail.com>
2020-02-13 12:41:32 -0500

    add prefix to different videos

--- python_scripts/text_rename.py
+++ python_scripts/text_rename.py
@@ -1,4 +1,3 @@
-
 import shutil
 import os
 import sys
@@ -9,17 +8,19 @@ Rename YOLO annotation txt files to match images names and offset count by 1
 """ 
 
 # directory with text files
-directory = "../final_annotations/BinCam2/7_BinCam2" 
+video = 'Annotation1'
+directory = '../../final_annotations/' + video + '/3_' + video
+prefix = 'c_'
 
 for filename in os.listdir(directory):
-	new_name = re.sub('frame_0+', '', filename)
+	new_name = re.sub('frame_0{2,3}', '', filename)
 	new_name = re.sub('.txt', '', new_name)
 
 	# ignore object names file
 	if(filename != 'obj.names'):
 		new_name = int(new_name)
 		new_name += 1
-		new_name = str(new_name) + ".txt"
-	os.rename(directory+"/"+filename, directory+"/"+new_name)
+		new_name = prefix + str(new_name) + '.txt'
+	os.rename(directory+'/'+filename, directory+'/'+new_name)
 
 
